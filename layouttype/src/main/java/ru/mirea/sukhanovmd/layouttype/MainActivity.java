2e8ead7055e13ed22cd542b41ab2bd667739cce2
unknown <rpz326@yandex.ru>
2024-03-05 22:58:36 +0300

    Initial commit

---
+++ layouttype/src/main/java/ru/mirea/sukhanovmd/layouttype/MainActivity.java
@@ -0,0 +1,28 @@
+package ru.mirea.sukhanovmd.layouttype;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        TextView myTextView = (TextView) findViewById(R.id.textView);
+        myTextView.setText("New text in MIREA");
+        Button button = findViewById(R.id.button20);
+        button.setText("MireaButton");
+        CheckBox checkBox = findViewById(R.id.checkBox2);
+        checkBox.setChecked(true);
+    }
+}
